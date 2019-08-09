6f3cca3afa734c5d5913862384ab061d98e3ffd6
Milen Khanal <milenkhanal@gmail.com>
2019-08-09 13:34:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
-  array.each_with_index{ |array, index| put "index"
-  array = index
+  array.each_with_index{ |array, index|
+  puts "index": #{index} "." {array}
   }
   return array
 end
