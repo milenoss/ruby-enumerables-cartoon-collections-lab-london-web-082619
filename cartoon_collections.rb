12445bce6da062ce1596988ba6dc90720a12c193
Milen Khanal <milenkhanal@gmail.com>
2019-08-09 15:06:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,12 @@ return array
 end
 
 def long_planeteer_calls (array)
-
+array.any? { |word| word.length >= 3 } #=> true
+%w[ant bear cat].any? { |word| word.length >= 4 } #=> true
+%w[ant bear cat].any?(/d/)                        #=> false
+[nil, true, 99].any?(Integer)                     #=> true
+[nil, true, 99].any?                              #=> true
+[].any?                                           #=> false
 
 
 def find_the_cheese# code an argument here
