da94720578ae8c9b3922af2d3e3034a579ded5f4
Milen Khanal <milenkhanal@gmail.com>
2019-08-09 13:42:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(array)
   return array
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.each{|n| n}
+  }
+return array
 end
 
 def long_planeteer_calls# code an argument here
