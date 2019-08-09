d366c0c82e67375af6d4a3f019e4af3549f6e3e5
Milen Khanal <milenkhanal@gmail.com>
2019-08-09 14:15:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-array.each { |cap| puts "#{cap}.capitalize + !}" }
+array.each { |cap| puts "#{cap.upcase!}" }
 return array
 end
 
