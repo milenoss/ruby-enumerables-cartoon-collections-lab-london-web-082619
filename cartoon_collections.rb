b5efced7db85c9aab9945464ec262cbfe47401f3
Milen Khanal <milenkhanal@gmail.com>
2019-08-09 13:35:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,10 @@
 def roll_call_dwarves(array)
-  array.each_with_index{ |array, index|
-  puts "index": #{index} "." {array}
+  array.each_with_index{ |v, index|
+  puts "index: #{index} "." #{v}"
   }
   return array
 end
-a=[11,22,31,224,44].each_with_index { |val,index| puts "index: #{index} for #{val}" if val < 30}
-  index: 0 for 11
-  index: 1 for 22
-  => [11, 22, 31, 224, 44]
+
 def summon_captain_planet# code an argument here
   # Your code here
 end
